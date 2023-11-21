c7bd5e872017397ef88b83b34736b80c6217a16c
AbeYuki <aim4highergg@gmail.com>
2023-11-21 20:56:20 +0900

    update

--- temp-exporter/main.py
+++ temp-exporter/main.py
@@ -6,21 +6,14 @@ import asyncio
 
 app = FastAPI()
 
+CPU_TEMP = Gauge('cpu_temperature', 'Temperature of the CPU')
+
 def fetch_metrics():
     # CPUの温度を返す関数
-    CPU_TEMP = Gauge('cpu_temperature', 'Temperature of the CPU')
     CPU_TEMP.set(psutil.sensors_temperatures()['cpu_thermal'][0].current)
     return {"message": "Metrics updated"}
 
 @app.get("/metrics")
 def get_metrics():
+    fetch_metrics() 
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
-
-@app.on_event("startup")
-async def start_periodic_metrics_update():
-    asyncio.create_task(periodic_metrics_update())
-
-async def periodic_metrics_update():
-    while True:
-        fetch_metrics()
-        await asyncio.sleep(60)
